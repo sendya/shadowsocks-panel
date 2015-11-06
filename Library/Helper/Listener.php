29a4b2c9a1c55b33af4c69a76d29578467fa244f
Sendya <18x@loacg.com>
2015-11-06 10:51:40 +0800

    Fix checkLogin

--- Library/Helper/Listener.php
+++ Library/Helper/Listener.php
@@ -12,18 +12,10 @@ class Listener {
     
     public static function checkLogin() {
         global $user;
-        $cookie = $_COOKIE['auth'];
-        if(!empty($cookie)) {
-            $auth = new User();
-            list($auth->uid, $auth->email, $auth->nickname) = explode("\t", Encrypt::decode(base64_decode($cookie), COOKIE_KEY));
-            if(!isset($auth) || $auth->uid == '' || $auth->email=='') {
-                return false;
-            }
+        $user = User::getInstance();
 
-            $user = User::GetUserByEmail($auth->email);
-            if($user->uid == $auth->uid) {
-                return true;
-            }
+        if(!$user) {
+            return true;
         }
         return false;
     }
