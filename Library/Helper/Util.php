a655e83c4737fcfa2d32091a14ef5a8e6b205b7e
Sendya <18x@loacg.com>
2015-12-10 00:05:07 +0800

    remove i @qq.com

--- Library/Helper/Util.php
+++ Library/Helper/Util.php
@@ -16,8 +16,10 @@ class Util
         $pattern = "/^([0-9A-Za-z\\-_\\.]+)@([0-9a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?)$/i";
         if (!preg_match($pattern, $chkMailAddress))
             return "邮箱地址格式不正确";
+        /*
         if (stristr($chkMailAddress, "qq.com") != false)
             return "该邮箱地址不被支持,请更换";
+        */
         if (\Model\User::GetUserByEmail($chkMailAddress) != false)
             return "邮箱地址已经被注册使用";
         return null;
