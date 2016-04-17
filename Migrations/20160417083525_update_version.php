8796a042444e4db5ae77bbcc300d8afd12ef1673
Sendya <18x@loacg.com>
2016-04-17 16:52:46 +0800

    Update Version

---
+++ Migrations/20160417083525_update_version.php
@@ -0,0 +1,21 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UpdateVersion extends AbstractMigration
+{
+    public function up()
+    {
+        // 2016-04-17 update version - by Sendya
+        $option = [
+            [
+                'k'     =>  'version',
+                'v'     =>  'v1.04'
+            ]
+        ];
+
+        $this->execute("DELETE FROM `options` WHERE `k` = 'version'");
+        $this->insert('options', $option);
+
+    }
+}
