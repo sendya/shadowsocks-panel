f89dc3e63d4fc0a06abdb25e37c731a038c36cc6
Sendya <18x@loacg.com>
2016-04-19 18:04:56 +0800

    Mailer post all user

--- Library/Controller/Admin/Mailer.php
+++ Library/Controller/Admin/Mailer.php
@@ -10,6 +10,7 @@ namespace Controller\Admin;
 
 
 use Core\Template;
+use Model\Mail;
 use Model\User;
 
 class Mailer
@@ -21,6 +22,29 @@ class Mailer
         $data['user'] = User::getCurrent();
         Template::setContext($data);
         Template::setView('admin/mailer');
+    }
+
+    public function postAll()
+    {
+        $subject = $_POST['mailer_subject'];
+        $content = $_POST['mailer_content'];
+        if($subject == null || $subject == '' || $content == null || $content == '') {
+            return array('error' => 1, 'message' => '请求错误，您提交的参数不对。');
+        }
+
+        $users = User::getUserList();
+
+        $mailer = new \Helper\Mailer();
+
+        $mail = new Mail();
+        $mail->subject = $subject;
+        $mail->content = $content;
+
+        foreach($users as $user) {
+            $mail->to = $user->email;
+            $mailer->send($mail);
+        }
+        return array('error' => 1, 'message' => '群邮件已经加入列队正在发送中..');
     }
 
 }
