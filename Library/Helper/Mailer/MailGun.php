e70c63971b99a13a77a8fd53cfccaa4f8f1e8928
Acris Liu <acrisliu@gmail.com>
2016-04-25 15:31:40 +0800

    fixed mailgun

--- Library/Helper/Mailer/MailGun.php
+++ Library/Helper/Mailer/MailGun.php
@@ -14,6 +14,8 @@ use Helper\Option;
 use Helper\Utils;
 use Model\Mail;
 use Mailgun\Mailgun as MG;
+use Http\Adapter\Guzzle6\Client;
+use ReflectionObject;
 
 /**
  * MailGun 邮件代发服务
@@ -35,7 +37,7 @@ class MailGun implements IMailer
             $_config = array(
                 "mailgun_key" => "key-********************",
                 'mailgun_domain' => 'post.mloli.com',
-                'from' => 'no-reply@'
+                'from' => '某科学的H本 <h@loacg.com>'
             );
             Option::set('MAIL_' . $className, json_encode($_config)); // 设定默认配置
             return false;
@@ -45,8 +47,9 @@ class MailGun implements IMailer
 
     public function send(Mail $mail)
     {
-        $mg = new MG($this->config['mailgun_key']);
-        $status = $mg->sendMessage($this->config['mailgun_domain'], array('from'    => $this->config['from'].$this->config['mailgun_domain'],
+        $client = new Client();
+        $mg = new MG($this->config['mailgun_key'], $client);
+        $status = $mg->sendMessage($this->config['mailgun_domain'], array('from'    => $this->config['from'],
             'to'      => $mail->to,
             'subject' => $mail->subject,
             'html'    => $mail->content));
@@ -56,5 +59,17 @@ class MailGun implements IMailer
         return true;
     }
 
+    public function __construct()
+    {
+        $className = Utils::getShortName($this);
+        $this->isAvailable();
 
+        $config = Option::get('MAIL_' . $className);
+        if (!$config) {
+            throw new Error("邮件模块 " . $className . " 配置不完整，无法使用。");
+        }
+
+        $this->config = json_decode($config, true);
+        Option::set('MAIL_' . $className, json_encode($this->config));
+    }
 }
