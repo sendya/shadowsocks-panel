4007bb21ea018c03255516ea5ace7c4f947acd06
Sendya <18x@loacg.com>
2016-04-27 17:25:47 +0800

    Update CRON clearLogs

--- Library/Helper/Cron/ClearLogs.php
+++ Library/Helper/Cron/ClearLogs.php
@@ -7,9 +7,7 @@
 namespace Helper\Cron;
 
 use Contactable\ICron;
-
-use Helper\Setting;
-use Model\User;
+use Core\Database;
 
 /**
  * 计划任务 - Clear Logs
@@ -20,12 +18,19 @@ use Model\User;
 class ClearLogs implements ICron
 {
 
-    const STEP = 3600; // 1小时执行一次
+    const STEP = 86400; // 1天执行一次
 
     public function run()
     {
+        // 清理一个月前的数据
+        $mon = time()-2592000;
+        $stn = Database::sql('DELETE FROM `card` WHERE add_time<? AND status=0');
+        $stn->bindValue(1, $mon, Database::PARAM_INT);
+        $stn->execute();
 
-
+        $stn = Database::sql("DELETE FROM `invite` WHERE dateLine<? AND status=1");
+        $stn->bindValue(1, $mon, Database::PARAM_INT);
+        $stn->execute();
     }
 
     public function getStep()
