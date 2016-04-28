6b9dedf0366d2fd02dbe7b9c1d8b41c71dd46838
Sendya <18x@loacg.com>
2016-04-28 14:40:25 +0800

    Update install

--- index.php
+++ index.php
@@ -74,21 +74,21 @@ function copyDir($strSrcDir, $strDstDir)
 
 function delDir($dir) {
     //先删除目录下的文件：
-    $dh=opendir($dir);
-    while ($file=readdir($dh)) {
+    $dh=@opendir($dir);
+    while ($file=@readdir($dh)) {
         if($file!="." && $file!="..") {
             $fullpath=$dir."/".$file;
             if(!is_dir($fullpath)) {
-                unlink($fullpath);
+                @unlink($fullpath);
             } else {
                 deldir($fullpath);
             }
         }
     }
 
-    closedir($dh);
+    @closedir($dh);
     //删除当前文件夹：
-    if(rmdir($dir)) {
+    if(@rmdir($dir)) {
         return true;
     } else {
         return false;
