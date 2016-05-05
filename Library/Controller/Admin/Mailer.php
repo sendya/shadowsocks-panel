a4615f3e0047173e652f5b7f4a2b922ba445a4cf
Sendya <18x@loacg.com>
2016-05-05 10:04:23 +0800

    Fix : mailer postAll

--- Library/Controller/Admin/Mailer.php
+++ Library/Controller/Admin/Mailer.php
@@ -82,9 +82,8 @@ class Mailer
      */
     public function postAll()
     {
-        $subject = daddslashes($_POST['mailer_subject']);
-        $content = daddslashes($_POST['mailer_content']);
-        $content = nl2br(htmlspecialchars($content));
+        $subject = $_POST['mailer_subject'];
+        $content = nl2br(htmlspecialchars($_POST['mailer_content']));
         $content .= "<p style=\"padding: 1.5em 1em 0; color: #999; font-size: 12px;\">—— 本邮件由 " . SITE_NAME . " (<a href=\"" . BASE_URL . "\">" . BASE_URL . "</a>) 管理员发送</p>";
         if ($subject == null || $subject == '' || $content == null || $content == '') {
             return array('error' => 1, 'message' => '请求错误，您提交的参数不对。');
