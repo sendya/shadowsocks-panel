c66a83b38bd9492867a3255bc91ed7b11718869f
Sendya <18x@loacg.com>
2017-08-04 23:55:37 +0800

    fixed install script

--- index.php
+++ index.php
@@ -269,8 +269,10 @@ switch ($argv[1]) {
 
                     }
                 }*/
-
-                exec("unzip $resourcePath");
+		$return_arr = [];
+		$resPath = ROOT_PATH . 'Resource/';
+                exec("unzip -o -d $resPath $resourcePath", $return_arr);
+		print_arr($return_arr);
             }
             echo 'Copying resources...';
             copyDir(ROOT_PATH . 'Resource', ROOT_PATH . 'Public/Resource');
